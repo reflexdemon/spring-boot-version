50e0a99c5dacbabeac22b6017487f8a7b4421006
vprasanna <vprasanna@bridge2solutions.com>
2018-06-13 13:17:37 -0400

    Moved the code to streams and Lambda.....

--- src/main/java/io/vpv/version/springbootversion/service/BootVersionService.java
+++ src/main/java/io/vpv/version/springbootversion/service/BootVersionService.java
@@ -8,8 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by vprasanna on 6/12/18.
@@ -19,7 +19,7 @@ public class BootVersionService {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     public List<Dependency> getDependencies(final String bootVersion) {
-        List<Dependency> dependencies = new ArrayList<>();
+        List<Dependency> dependencies = null;
         try {
             logger.debug("Searching dependencies for {}", bootVersion);
             String url = "https://docs.spring.io/spring-boot/docs/"
@@ -30,17 +30,21 @@ public class BootVersionService {
             document = Jsoup.connect(url).get();
             Elements allTables =
                     document.select(".informaltable");
-            allTables.select("tr")
-                    .forEach(row -> {
-                        List<String> values = row.select("td").eachText();
+            dependencies = allTables.select("tr").parallelStream().map(
+                    element -> {
+                        List<String> values = element.select("td").eachText();
                         if (values.size() >= 3) {
                             String groupId = values.get(0);
                             String artifactId = values.get(1);
                             String version = values.get(2);
                             Dependency dependency = new Dependency(bootVersion, groupId, artifactId, version);
-                            dependencies.add(dependency);
+//                            dependencies.add(dependency);
+                            return dependency;
                         }
-                    });
+                        return null;
+                    }
+            ).filter(item -> item != null)
+                    .collect(Collectors.toList());
         } catch (Exception  e) {
             logger.error("Problem while getting the dependencies for {}", bootVersion, e);
             throw new RuntimeException("Problem while getting the dependencies for " + bootVersion, e);
