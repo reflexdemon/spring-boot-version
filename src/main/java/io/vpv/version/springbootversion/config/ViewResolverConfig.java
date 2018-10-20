f87c93f396989ef7ba286b0ed0c685839bf5f2be
reflexdemon <venkateswara.vp@gmail.com>
2018-10-20 07:54:27 -0400

    Introducing Tiles

---
+++ src/main/java/io/vpv/version/springbootversion/config/ViewResolverConfig.java
@@ -0,0 +1,32 @@
+package io.vpv.version.springbootversion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.result.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@Configuration
+public class ViewResolverConfig implements WebMvcConfigurer {
+    @Bean
+    public UrlBasedViewResolver viewResolver() {
+        UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver();
+        tilesViewResolver.setViewClass(TilesView.class);
+        return tilesViewResolver;
+    }
+
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tiles = new TilesConfigurer();
+        tiles.setDefinitions(new String[] { "/ui/tiles/tiles.xml" });
+        return tiles;
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/*.js/**").addResourceLocations("/ui/static/");
+        registry.addResourceHandler("/*.css/**").addResourceLocations("/ui/static/");
+    }
+}
---
+++ src/main/java/io/vpv/version/springbootversion/controller/page/HomeController.java
@@ -0,0 +1,13 @@
+package io.vpv.version.springbootversion.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping (value = "/")
+    public String landingPage() {
+        return "base";
+    }
+}
