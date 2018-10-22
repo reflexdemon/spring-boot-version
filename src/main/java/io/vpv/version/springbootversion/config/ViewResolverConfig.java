54542b807160f564c3dfb57978e43808e023b718
reflexdemon <venkateswara.vp@gmail.com>
2018-10-22 06:32:11 -0400

    Major code cleanup after adding tiles. got the junit pass.

--- src/main/java/io/vpv/version/springbootversion/config/ViewResolverConfig.java
+++ src/main/java/io/vpv/version/springbootversion/config/ViewResolverConfig.java
@@ -2,9 +2,9 @@ package io.vpv.version.springbootversion.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.result.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 
@@ -20,7 +20,7 @@ public class ViewResolverConfig implements WebMvcConfigurer {
     @Bean
     public TilesConfigurer tilesConfigurer() {
         TilesConfigurer tiles = new TilesConfigurer();
-        tiles.setDefinitions(new String[] { "/ui/tiles/tiles.xml" });
+        tiles.setDefinitions("/ui/tiles/tiles.xml");
         return tiles;
     }
 
